797cdfbc10b2cd59768eb45d2536c0f48599d56e
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 10:59:50 -0400

    add lite server config and app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// data types : numbers, strings, booleans
+
+function add (n1, n2) {
+    return n1 + n2
+}
+
+const number1 = 5
+const number2 = 2.4
+
+// store numbers as arguments 
+const result = add(number1, number2)
+console.log(result)

92531d1cd14b42e8d776196ad413835b02faa98e
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 11:48:28 -0400

    add array and hobbies

--- app.ts
+++ app.ts
@@ -4,7 +4,11 @@
 // } = {
 const person = {    
     name: 'Kion',
-    age: 1
+    age: 1,
+    hobbies: ['Eating Meat', 'Attacking Moma feet']
 }
+
+let favoriteActivities: string[]
+favoriteActivities = ['Eating Meat', 1]
 
 console.log(person.name)

4219b986e7d8ef5d36a84fba0d4d3d477e9dc91c
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 10:02:39 -0400

    add vanilla javascrupt typeof check if numbers are numbers to avoid value string error

--- js-only.js
+++ js-only.js
@@ -3,7 +3,11 @@ const input1 = document.getElementById("num1")
 const input2 = document.getElementById("num2")
 
 function add(num1, num2) {
+    if (typeof num1 === "number" && typeof num2 === "number") {
     return num1 + num2
+    } else {
+        return +num1 + +num2
+    }
 }
 
 // logical error in the application - VALUE of an input element is always a string!

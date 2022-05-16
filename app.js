c7dcc12c6d255b01a8a47a20bcdfe3fb3ecb0d21
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 12:07:47 -0400

    add identifier

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 // const person: {
 //    name: string
-//    age: number 
+//    age: number
 // } = {
+//const person: {
+//name: string;
+//age: number;
+//hobbies: string[];
+//role: [number, string]; // tuple type
+//} = {
+//name: "Kion",
+//age: 1,
+//hobbies: ["Eating Meat", "Attacking Moma feet"],
+// this is tuple and it must only have two elements - tuple is an array with fixed element length
+//role: [2, "king"],
+//};
+var KING = 0;
+var READ_ONLY = 1;
+var prince = 2;
 var person = {
     name: 'Kion',
     age: 1,
-    hobbies: ['Eating Meat', 'Attacking Moma feet']
+    hobbies: ['Catching Mice', 'Eating Meat'],
+    role: KING
 };
+// person.role.push('prince')
+// person.role[1] = 10
+// person.role = [0, 'admin', 'user']
 // any type - it is flexible but don't use it often because you will lose all benefits of TS
 var favoriteActivities;
-favoriteActivities = ['Eating Meat'];
+favoriteActivities = ["Eating Meat"];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
+    // console.log(hobby.map()) - error - map() does not work on strings - TS shines here!
+}
+if (person.role === KING) {
+    console.log('is king');
 }
--- app.ts
+++ app.ts
@@ -1,30 +1,46 @@
 // const person: {
 //    name: string
-//    age: number 
+//    age: number
 // } = {
-const person: {
-    name: string
-    age: number
-    hobbies: string[]
-    role: [number, string] // tuple type
-} = {    
+//const person: {
+  //name: string;
+  //age: number;
+  //hobbies: string[];
+  //role: [number, string]; // tuple type
+//} = {
+  //name: "Kion",
+  //age: 1,
+  //hobbies: ["Eating Meat", "Attacking Moma feet"],
+  // this is tuple and it must only have two elements - tuple is an array with fixed element length
+  //role: [2, "king"],
+//};
+
+const KING = 0
+const READ_ONLY = 1
+const prince = 2
+
+const person = {
     name: 'Kion',
     age: 1,
-    hobbies: ['Eating Meat', 'Attacking Moma feet'],
-    // this is tuple and it must only have two elements - tuple is an array with fixed element length
-    role: [2, 'king']
+    hobbies: ['Catching Mice', 'Eating Meat'],
+    role: KING
 }
 
-person.role.push('prince')
-person.role[1] = 10
+// person.role.push('prince')
+// person.role[1] = 10
+// person.role = [0, 'admin', 'user']
 
 // any type - it is flexible but don't use it often because you will lose all benefits of TS
-let favoriteActivities: string[]
-favoriteActivities = ['Eating Meat']
+let favoriteActivities: string[];
+favoriteActivities = ["Eating Meat"];
 
-console.log(person.name)
+console.log(person.name);
 
 for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase())
-    // console.log(hobby.map()) - error - map() does not work on strings - TS shines here!
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()) - error - map() does not work on strings - TS shines here!
+}
+
+if (person.role === KING) {
+  console.log('is king')
 }

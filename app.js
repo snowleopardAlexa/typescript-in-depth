4ad54952640f798ada402a66f251714d3a30728f
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 11:20:56 -0400

    add downside note

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // data types : numbers, strings, booleans
 function add(n1, n2) {
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('Incorrect input!');
+    }
     return n1 + n2;
 }
 // 5 is a string and js concatenates two values
--- app.ts
+++ app.ts
@@ -1,6 +1,9 @@
 // data types : numbers, strings, booleans
 
 function add(n1: number , n2: number) {
+    // this method has downside - we can prevent this error with ts
+    // js - dynamically typed - they can change during runtime
+    // ts - statically typed 
     if (typeof n1 !== 'number' || typeof n2 !== 'number') {
         throw new Error('Incorrect input!')
     }
@@ -10,7 +13,7 @@ function add(n1: number , n2: number) {
 // 5 is a string and js concatenates two values
 // output 52.4 - logical error 
 // it hard to catch these errors 
-const number1 = 5
+const number1 = '5'
 const number2 = 2.4
 
 // store numbers as arguments 

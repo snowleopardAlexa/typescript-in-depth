8ac015773b4f5af998db58818d8823a4e01812f8
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 11:15:24 -0400

    add number type to code to specify the that variable should be a number

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 // data types : numbers, strings, booleans
-
-function add (n1, n2) {
-    return n1 + n2
+function add(n1, n2) {
+    return n1 + n2;
 }
-
 // 5 is a string and js concatenates two values
 // output 52.4 - logical error 
 // it hard to catch these errors 
-const number1 = '5'
-const number2 = 2.4
-
+var number1 = '5';
+var number2 = 2.4;
 // store numbers as arguments 
-const result = add(number1, number2)
-console.log(result)
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,2 +1,16 @@
-console.log("Your code goes here...")
+// data types : numbers, strings, booleans
+
+function add(n1: number , n2: number) {
+    return n1 + n2
+}
+
+// 5 is a string and js concatenates two values
+// output 52.4 - logical error 
+// it hard to catch these errors 
+const number1 = 5
+const number2 = 2.4
+
+// store numbers as arguments 
+const result = add(number1, number2)
+console.log(result)
 

c1f223972f66a3c3c8a16bcec49fcd33a7f53027
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 11:02:19 -0400

    change number1 from number to string

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ function add (n1, n2) {
     return n1 + n2
 }
 
-const number1 = 5
+// 5 is a string and js concatenates two values
+// output 52.4 - logical error 
+// it hard to catch these errors 
+const number1 = '5'
 const number2 = 2.4
 
 // store numbers as arguments 

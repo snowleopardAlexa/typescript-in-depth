f9fb75628bda98f381dee6c9fbeb6e88a4aa48f7
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 13:05:00 -0400

    add literal types

--- app.js
+++ app.js
@@ -1,47 +1,16 @@
-// const person: {
-//    name: string
-//    age: number
-// } = {
-//const person: {
-//name: string;
-//age: number;
-//hobbies: string[];
-//role: [number, string]; // tuple type
-//} = {
-//name: "Kion",
-//age: 1,
-//hobbies: ["Eating Meat", "Attacking Moma feet"],
-// this is tuple and it must only have two elements - tuple is an array with fixed element length
-//role: [2, "king"],
-//};
-//const KING = 0
-//const READ_ONLY = 1
-//const prince = 2
-// assigns labels to numbers
-var Role;
-(function (Role) {
-    Role[Role["KING"] = 0] = "KING";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["PRICE"] = 2] = "PRICE";
-})(Role || (Role = {}));
-var person = {
-    name: 'Kion',
-    age: 1,
-    hobbies: ['Catching Mice', 'Eating Meat'],
-    role: Role.KING
-};
-// person.role.push('prince')
-// person.role[1] = 10
-// person.role = [0, 'admin', 'user']
-// any type - it is flexible but don't use it often because you will lose all benefits of TS
-var favoriteActivities;
-favoriteActivities = ["Eating Meat"];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()) - error - map() does not work on strings - TS shines here!
+// union types
+// we can't combine both numbers and strings with + - union types can help with that
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-if (person.role === Role.KING) {
-    console.log('is king');
-}
+var combinedAges = combine(40, 30);
+console.log(combinedAges);
+var combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
--- app.ts
+++ app.ts
@@ -1,17 +1,29 @@
-// union types
-// we can't combine both numbers and strings with + - union types can help with that
-function combine(input1: number | string, input2: number | string)  {
-    let result
-    if (typeof input1 === 'number'  && typeof input2 === 'number') {
-        result = input1 + input2
-    } else {
-        result = input1.toString() + input2.toString()
-    }
-    return result
-}
+// literal types
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultConversion: 'as-number' | 'as-text'
+) {
+  let result
+  if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === 'as-number' {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result
+  //if (resultConversion === 'as-number') {
+    //  return + result
+  //} else {
+  //    return result.toString()
+ // }
+  //return result;
+//}
 
-const combinedAges = combine(40, 30)
-console.log(combinedAges)
+const combinedAges = combine(40, 30, "as-number");
+console.log(combinedAges);
 
-const combinedNames = combine('Max', 'Anna')
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges)
+
+const combinedNames = combine("Max", "Anna", "as-text");
 console.log(combinedNames)

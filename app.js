783cdce89e6abb42a8d06b633af623772118c00a
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 15:29:40 -0400

    add never and error function

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-function add(n1, n2) {
-    return n1 + n2;
+// unknown types
+// unknown is different to any
+// it is better choice than any 
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Kion';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-// void - does not return anything, it does not have return statement
-// the void return type
-// undefined is a valid type in typescript
-// void is a valid type in typescript
-function printResult(num) {
-    console.log('Result: ' + num);
+// never type 
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
 }
-printResult(add(5, 12));
-var combineValues;
-combineValues = add;
-combineValues = printResult;
-// it throws error combineValues is not a function
-// combineValues =  5
-console.log(combineValues(8, 8));
+var result = generateError('An error occurred!', 500);
+console.log(result);
--- app.ts
+++ app.ts
@@ -11,3 +11,12 @@ if (typeof userInput === 'string') {
     userName = userInput
 }
 
+// never type 
+// this function always crashes the script because it throws error
+// add never because it tells devs that the function is intended to return nothing
+function generateError(message: string, code: number): never {
+    throw { message: message, errorCode: code }
+}
+
+const result = generateError('An error occurred!', 500)
+console.log(result)

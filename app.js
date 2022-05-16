2fcbe138018684fe988aa7aeed6cec6d24cd9b14
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 11:26:45 -0400

    add truthy and false value, and 3rd argument, remove console.log

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 // data types : numbers, strings, booleans
-function add(n1, n2) {
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('Incorrect input!');
+function add(n1, n2, showResult) {
+    // this method has downside - we can prevent this error with ts
+    // js - dynamically typed - they can change during runtime
+    // ts - statically typed - set during development
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //    throw new Error('Incorrect input!')
+    // }
+    // truthy or falsy value
+    if (showResult) {
+        console.log(n1 + n2);
     }
-    return n1 + n2;
+    else {
+        return n1 + n2;
+    }
 }
 // 5 is a string and js concatenates two values
 // output 52.4 - logical error 
 // it hard to catch these errors 
-var number1 = '5';
+var number1 = 5;
 var number2 = 2.4;
+var printResult = true;
 // store numbers as arguments 
-var result = add(number1, number2);
-console.log(result);
+add(number1, number2, printResult);
--- app.ts
+++ app.ts
@@ -1,22 +1,29 @@
 // data types : numbers, strings, booleans
 
-function add(n1: number , n2: number) {
+function add(n1: number , n2: number, showResult: boolean) {
     // this method has downside - we can prevent this error with ts
     // js - dynamically typed - they can change during runtime
-    // ts - statically typed 
-    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-        throw new Error('Incorrect input!')
+    // ts - statically typed - set during development
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //    throw new Error('Incorrect input!')
+    // }
+    
+    // truthy or falsy value
+    if (showResult) {
+      console.log(n1 + n2)
+    } else {
+        return n1 + n2
     }
-    return n1 + n2
 }
 
 // 5 is a string and js concatenates two values
 // output 52.4 - logical error 
 // it hard to catch these errors 
-const number1 = '5'
+const number1 = 5
 const number2 = 2.4
+const printResult = true
 
 // store numbers as arguments 
-const result = add(number1, number2)
-console.log(result)
+add(number1, number2, printResult)
+
 

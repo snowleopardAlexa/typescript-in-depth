82254bb7b3c9c6d2710234a98a97e1614ed97b1b
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 15:16:16 -0400

    add callback function with TS

--- app.ts
+++ app.ts
@@ -10,15 +10,30 @@ function printResult(num: number): void {
     console.log('Result: ' + num)
 }
 
+// callback function
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2
+    cb(result)
+}
+
+// let someValue: undefined 
+addAndHandle(10, 20, (result) => {
+
+})
+
 printResult(add(5, 12))
 
-let combineValues: Function
+// function types - which type of function we want to use
+let combineValues: (a: number, b: number) => number
 
 combineValues = add 
-combineValues = printResult
+// combineValues = printResult
 
 // it throws error combineValues is not a function
 // combineValues =  5
 
 console.log(combineValues(8,8))
 
+addAndHandle(10, 20, (result) => {
+    console.log(result)
+})

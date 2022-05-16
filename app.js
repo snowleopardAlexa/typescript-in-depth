ac91ccdc3de6796eeb7001a69d27bed80d04d98d
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 11:40:23 -0400

    add object

--- app.js
+++ app.js
@@ -1,26 +1,5 @@
-// data types : numbers, strings, booleans
-function add(n1, n2, showResult, phrase) {
-    // this method has downside - we can prevent this error with ts
-    // js - dynamically typed - they can change during runtime
-    // ts - statically typed - set during development
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //    throw new Error('Incorrect input!')
-    // }
-    var result = n1 + n2;
-    // truthy or falsy value
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-// 5 is a string and js concatenates two values
-// output 52.4 - logical error 
-// it hard to catch these errors 
-var number1 = 5;
-var number2 = 2.4;
-var printResult = true;
-var resultPhrase = 'Result is: ';
-// store numbers as arguments 
-add(number1, number2, printResult, resultPhrase);
+var person = {
+    name: 'Kion',
+    age: 1
+};
+console.log(person.nickname);
--- app.ts
+++ app.ts
@@ -1,32 +1,6 @@
-// data types : numbers, strings, booleans
-
-function add(n1: number , n2: number, showResult: boolean, phrase: string) {
-    // this method has downside - we can prevent this error with ts
-    // js - dynamically typed - they can change during runtime
-    // ts - statically typed - set during development
-    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-    //    throw new Error('Incorrect input!')
-    // }
-    
-
-    const result = n1 + n2
-    // truthy or falsy value
-    if (showResult) {
-      console.log(phrase + result)
-    } else {
-        return result
-    }
+const person: object = {
+    name: 'Kion',
+    age: 1
 }
 
-// 5 is a string and js concatenates two values
-// output 52.4 - logical error 
-// it hard to catch these errors 
-const number1 = 5
-const number2 = 2.4
-const printResult = true
-const resultPhrase = 'Result is: '
-
-// store numbers as arguments 
-add(number1, number2, printResult, resultPhrase)
-
-
+console.log(person.nickname)

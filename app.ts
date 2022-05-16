18dfd4e270830fea1e6328fca3c698184b258334
snowleopardAlexa <alexaslomski@gmail.com>
2022-05-16 12:33:25 -0400

    add if statement typeof check

--- app.ts
+++ app.ts
@@ -1,7 +1,12 @@
 // union types
 // we can't combine both numbers and strings with + - union types can help with that
 function combine(input1: number | string, input2: number | string)  {
-    const result = input1 + input2 
+    let result
+    if (typeof input1 === 'number'  && typeof input2 === 'number') {
+        result = input1 + input2
+    } else {
+        result = input1.toString() + input2.toString()
+    }
     return result
 }
 
@@ -9,3 +14,4 @@ const combinedAges = combine(40, 30)
 console.log(combinedAges)
 
 const combinedNames = combine('Max', 'Anna')
+console.log(combinedNames)
